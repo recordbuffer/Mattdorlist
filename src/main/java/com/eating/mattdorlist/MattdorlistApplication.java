0d6114ab8c2b1bd8778a49e316e641b784ba69f5
MEE <76642597+recordbuffer@users.noreply.github.com>
2023-05-03 12:48:28 +0900

    Add Project Settings

---
+++ src/main/java/com/eating/mattdorlist/MattdorlistApplication.java
@@ -0,0 +1,13 @@
+package com.eating.mattdorlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattdorlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattdorlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eating/mattdorlist/MattdorlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eating.mattdorlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattdorlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
